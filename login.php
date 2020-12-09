4733b1c2aa237243106eb3c90ba80067f214261d
rick000lu <rick000.lu@gmail.com>
2020-12-09 21:16:09 +0800

    login system

--- login.php
+++ login.php
@@ -1,7 +1,18 @@
 <html>
 <body>
 <title>login system</title>
-
-<?php  ?>
+<form method="post" action="<?php echo"$_SERVER['PHP_SELF'];" ?>">
+  Address: <input type="text" name="Address">
+  Password: <input type="Password" name="pwd">
+  <input type="submit">
+</form>
+<?php
+if($_SERVER['REQUEST_METHOD']=="post")
+{
+  $add=$_POST['Address'];
+  $pwd=$_POST['pwd'];
+  echo $add;
+}
+?>
 </body>
 </html>
