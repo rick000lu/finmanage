7da61a7af917843603106111f9adb7a83c3fb953
rick000lu <rick000.lu@gmail.com>
2020-12-09 21:36:47 +0800

    login page

--- login.php
+++ login.php
@@ -1,17 +1,19 @@
 <html>
 <body>
 <title>login system</title>
-<form method="post" action="<?php echo"$_SERVER['PHP_SELF'];" ?>">
-  Address: <input type="text" name="Address">
-  Password: <input type="Password" name="pwd">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+  Address: <input type="text" name="Address"><br>
+  Password: <input type="Password" name="pwd"><br>
   <input type="submit">
 </form>
 <?php
-if($_SERVER['REQUEST_METHOD']=="post")
+if($_SERVER['REQUEST_METHOD']=="POST")
 {
   $add=$_POST['Address'];
   $pwd=$_POST['pwd'];
-  echo $add;
+  echo $add."<br>";
+  $pwd=hash('sha256',$pwd);
+  echo $pwd;
 }
 ?>
 </body>
